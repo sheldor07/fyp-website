e7dcf0a248883939f1e23d5375000fb55ba0a9cc
Yajat Gulati <yajatgulati01@gmail.com>
2025-04-03 14:28:16 +0800

    posthog update config

--- frontend/app/providers.js
+++ frontend/app/providers.js
@@ -12,8 +12,7 @@ export function PostHogProvider({ children }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
-      person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
-      capture_pageview: false // Disable automatic pageview capture, as we capture manually
+      person_profiles: "always",
     })
   }, [])
 
